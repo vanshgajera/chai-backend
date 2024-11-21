bc5763676546b9002645899b2c35c012a104c400
vanshgajera <vanshgajera50@gmail.com>
2024-11-21 22:32:20 +0530

    add subscription inner to toggleSubscription ToDo create to now

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,8 +7,42 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    // const {channelId} = req.params
     // TODO: toggle subscription
+
+     const {channelId} = req.params
+     const { sub } = req.query // Check the query parameter for subscription action
+                                // If sub === "true", the user will unsubscribe.
+                                // If sub !== "true", the user will subscribe.
+
+     if(!channelId) {
+        throw new ApiError(500, "channelId is required")
+     }
+
+     if(sub === "true") {
+        await Subscription.deleteOne(
+            {
+                channel: channelId,
+                subscriber: req.user?._id
+            }
+        )
+     }
+     else{
+        await Subscription.create(
+            {
+                subscriber: req.user?._id,
+                channel: channelId
+            }
+        )
+        console.log(Subscription);
+     }
+
+     
+
+     return res
+     .status(201)
+    .json(new ApiResponse(200, {}, "Subscription toggle successfully"))
+    
 })
 
 // controller to return subscriber list of a channel
